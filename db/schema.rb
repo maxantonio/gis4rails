a9c3806e594b1f7cc56d63a3c98dc34360c48ccb
Julio Cesar <jcesar@jovenclub.cu>
2015-06-29 15:44:02 -0700

    agregado campo publicado a la base de datos OK

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150629212533) do
+ActiveRecord::Schema.define(version: 20150629224209) do
 
   create_table "documents", force: :cascade do |t|
     t.integer  "company_id"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20150629212533) do
     t.date     "release_date"
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
+    t.boolean  "publicado"
   end
 
   create_table "users", force: :cascade do |t|
